9e2a040b57350fceb5d363492493f75dd9e56d0b
Jackson Bailey <jackson.robert.bailey@gmail.com>
2023-07-02 19:49:01 -0400

    Combine common code into one place

--- pngstuff.py
+++ pngstuff.py
@@ -1,5 +1,6 @@
+from abc import ABC, abstractmethod
 from enum import Enum
-from typing import BinaryIO
+from typing import BinaryIO, Optional
 import click
 
 
@@ -37,28 +38,50 @@ _actions = list(map(lambda e: e.value, Action))
     help="Action to take",
     default="dump",
 )
-def cli_command(input: BinaryIO, output: BinaryIO, action: str) -> None:
+def cli_command(input: BinaryIO, output: Optional[BinaryIO], action: str) -> None:
     pngstuff_action(input=input, output=output, action=Action(action))
 
 
-def pngstuff_action(input: BinaryIO, output: BinaryIO, action: Action) -> None:
+def pngstuff_action(
+    input: BinaryIO, output: Optional[BinaryIO], action: Action
+) -> None:
     match action:
         case Action.DUMP:
-            dump(input=input)
+            walk_file(input=input, consumer=PngDumper())
         case Action.STRIP:
-            strip(input=input, output=output)
+            assert output is not None, "Expected somewhere to send output"
+            walk_file(input=input, consumer=PngStripper(output=output))
 
 
-def dump(input: BinaryIO) -> None:
-    header = input.read(8)
-    assert header == b"\x89PNG\r\n\x1a\n"
-    while True:
-        chunk_length = int.from_bytes(input.read(4), byteorder="big")
-        if not chunk_length:
-            break
-        chunk_type = input.read(4)
-        chunk_data = input.read(chunk_length)
-        chunk_crc = input.read(4)
+class PngConsumer(ABC):
+    @abstractmethod
+    def consumeHeader(self, header: bytes) -> None:
+        assert header == b"\x89PNG\r\n\x1a\n", "Improper file header '{header}'"
+
+    @abstractmethod
+    def consumeChunk(
+        self,
+        chunk_length_bytes: bytes,
+        chunk_length: int,
+        chunk_type: bytes,
+        chunk_data: bytes,
+        chunk_crc: bytes,
+    ) -> None:
+        pass
+
+
+class PngDumper(PngConsumer):
+    def consumeHeader(self, header: bytes) -> None:
+        super()
+
+    def consumeChunk(
+        self,
+        chunk_length_bytes: bytes,
+        chunk_length: int,
+        chunk_type: bytes,
+        chunk_data: bytes,
+        chunk_crc: bytes,
+    ) -> None:
         if chunk_type == b"IDAT":
             click.echo(
                 f"{chunk_type}\t{chunk_length}\t(omitted image data)\t{chunk_crc}"
@@ -67,10 +90,33 @@ def dump(input: BinaryIO) -> None:
             click.echo(f"{chunk_type}\t{chunk_length}\t{chunk_data}\t{chunk_crc}")
 
 
-def strip(input: BinaryIO, output: BinaryIO) -> None:
-    header = input.read(8)
-    assert header == b"\x89PNG\r\n\x1a\n"
-    output.write(header)
+class PngStripper(PngConsumer):
+    def __init__(self, output: BinaryIO):
+        self.output = output
+
+    def consumeHeader(self, header: bytes) -> None:
+        super()
+        self.output.write(header)
+
+    def consumeChunk(
+        self,
+        chunk_length_bytes: bytes,
+        chunk_length: int,
+        chunk_type: bytes,
+        chunk_data: bytes,
+        chunk_crc: bytes,
+    ) -> None:
+        if chunk_type not in [b"IHDR", b"IDAT"]:
+            click.echo(f"{chunk_type}\t{chunk_length}\t{chunk_data}\t{chunk_crc}")
+        else:
+            self.output.write(chunk_length_bytes)
+            self.output.write(chunk_type)
+            self.output.write(chunk_data)
+            self.output.write(chunk_crc)
+
+
+def walk_file(input: BinaryIO, consumer: PngConsumer) -> None:
+    consumer.consumeHeader(header=input.read(8))
     while True:
         chunk_length_bytes = input.read(4)
         chunk_length = int.from_bytes(chunk_length_bytes, byteorder="big")
@@ -79,10 +125,10 @@ def strip(input: BinaryIO, output: BinaryIO) -> None:
         chunk_type = input.read(4)
         chunk_data = input.read(chunk_length)
         chunk_crc = input.read(4)
-        if chunk_type not in [b"IHDR", b"IDAT"]:
-            click.echo(f"{chunk_type}\t{chunk_length}\t{chunk_crc}")
-        else:
-            output.write(chunk_length_bytes)
-            output.write(chunk_type)
-            output.write(chunk_data)
-            output.write(chunk_crc)
+        consumer.consumeChunk(
+            chunk_length_bytes=chunk_length_bytes,
+            chunk_length=chunk_length,
+            chunk_type=chunk_type,
+            chunk_data=chunk_data,
+            chunk_crc=chunk_crc,
+        )
